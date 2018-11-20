8c8eaba9138d42c677f857dea4725d868b345f47
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-11-20 19:00:53 +0900

    second commit

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,2 @@
-server '160.16.124.99', user: 'seiyaendo', roles: %w{app db web}
+server '160.16.124.99:50830', user: 'seiyaendo', roles: %w{app db web}
 set :ssh_options, keys: '/Users/endoseiya/.ssh/id_rsa'
