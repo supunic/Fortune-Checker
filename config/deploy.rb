360c30430cd5898f47b90d462b33ff2513470e86
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-11-20 18:21:32 +0900

    init unicorn

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,7 @@ lock '3.6.0'
 set :application, 'uranai_app'
 
 # cloneするgitのレポジトリ（xxxxxxxx：ユーザ名、yyyyyyyy：アプリケーション名）
-set :repo_url, 'https://github.com/xxxxxxxxx/yyyyyyyyy'
+set :repo_url, 'https://github.com/seiyaendo8/uranai_app.git'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
 set :branch, ENV['BRANCH'] || 'master'
