b8164d20ac9b59d2d4a2e6203bd0704af858905e
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2018-12-04 13:32:54 +0900

    fixed TotalRankingJob

--- app/jobs/total_ranking_tweet_job.rb
+++ app/jobs/total_ranking_tweet_job.rb
@@ -68,9 +68,9 @@ class TotalRankingTweetJob < ApplicationJob
       com = ["何かにチャレンジするなら今日かも！", "めっちゃいいかんじ！", "何かいいことがあるかも！", "努力が報われる日かも！", "今日は一日ハッピーな日です！"]
       comment = com.sample
 
-      text = "【今日の占い総合ナンバーワンは！？】\n\n・・・★#{@overall_top3[0]}★のあなたです！\n\n#{string}\n#{comment}\n\n#今朝の占い\n\nもっと詳しく↓https://fortune-checker.supunic.com"
+      text = "【今日の占い総合ナンバーワンは？！】\n\n・・・★#{@overall_top3[0]}★のあなたです！\n\n#{string}\n#{comment}\n\n#今朝の占い\n\n他の星座もチェック！↓https://fortune-checker.supunic.com"
       client.update(text)
-      
+
       Slack.chat_postMessage(
         channel: '#占いapp',
         text: "FortuneChecker\n12時のツイートが実行されました。"
