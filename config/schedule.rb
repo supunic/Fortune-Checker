b67fc6cec276ee7b58a738ea2bf7864387d11d52
遠藤聖也 <endoseiya@endoukiyoshinarinoMacBook-puro.local>
2019-01-06 20:56:11 +0900

    fixed schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,19 +2,19 @@ set :output, "log/scraping.log"
 set :environment, :production
 
 # スクレイピング処理
-# every :weekday, at: '8:05 am' do
-#   runner 'FortuneScrapingJob.perform_later'
-# end
+every :weekday, at: '8:05 am' do
+  runner 'FortuneScrapingJob.perform_later'
+end
 
 # ツイート処理 → スクレイピング処理成功時に移動
-# every :weekday, at: '8:08 am' do
-#   runner 'TweetJob.perform_later'
-# end
+every :weekday, at: '8:08 am' do
+  runner 'TweetJob.perform_later'
+end
 
 # 総合一位ツイート処理（追加）
-# every :weekday, at: '12:00 pm' do
-#   runner 'TotalRankingTweetJob.perform_later'
-# end
+every :weekday, at: '12:00 pm' do
+  runner 'TotalRankingTweetJob.perform_later'
+end
 
 # データベース削除処理
 every :weekday, at: '8:10 am' do
