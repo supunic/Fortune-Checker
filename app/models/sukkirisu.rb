540fba869215cba8784af07f989fd7a0527674c8
seiya endo <seiya.endo0830@gmail.com>
2020-06-06 19:01:23 +0900

    スッキリす占いスクレイピング修正

--- app/models/sukkirisu.rb
+++ app/models/sukkirisu.rb
@@ -1,49 +1,35 @@
 class Sukkirisu < ApplicationRecord
   # スッキりす誕生月占い（スッキリ/日本テレビ/月〜金/ ）
   def self.sukkirisuFortune(month)
-    page = Faraday.get('http://www.ntv.co.jp/sukkiri/sukkirisu/index.html')
+    page    = Faraday.get('https://www.ntv.co.jp/sukkiri/sukkirisu/index.html')
     page_ja = page.body.toutf8
 
-    n = page_ja.index('type1')
-    number = page_ja.rindex('div', n)
-    month_num = page_ja.index('month'+month.to_s+'"', number)
-    rank2_num = page_ja.index('2位', number)
-    rank11_num = page_ja.index('11位', number)
+    month_index          = page_ja.index('<span>'+month.to_s+'</span>月')
+    
+    tyosukkirisu_index     = page_ja.index('超スッキリす')
+    month_row1_index     = page_ja.rindex('row1', month_index)
+    month_row2_index     = page_ja.index('row2', month_index)
 
-    color_num = page_ja.index('color', month_num)
-    start_num = page_ja.index('>', color_num)
-    end_num = page_ja.index('<', start_num)
-    @color = page_ja.slice(start_num+1..end_num-1)
+    month_row2_p_index        = page_ja.index('<p>', month_row2_index)
+    month_row2_div_index      = page_ja.index('<div', month_row2_index)
 
-    if rank2_num < color_num && month_num < rank11_num
-      rankTxt_num = page_ja.index('rankTxt', month_num)
-      start_num = page_ja.index('>', rankTxt_num)
-      end_num = page_ja.index('<', start_num)
-      @rankTxt = page_ja.slice(start_num+1..end_num-1)
+    month_row1_rank_index_start     = page_ja.index('>', month_row1_index + 10)
+    month_row1_rank_index_end       = page_ja.index('位', month_row1_index)
+    month_row2_p_index_start     = page_ja.index('>', month_row2_p_index + 1)
+    month_row2_p_index_end       = page_ja.index('<', month_row2_p_index + 1)
+    month_row2_div_index_start   = page_ja.index('>', month_row2_div_index + 1)
+    month_row2_div_index_end     = page_ja.index('<', month_row2_div_index + 1)
 
-      rankTxt_p_num = page_ja.index('p', rankTxt_num+15)
-      start_num = page_ja.index('>', rankTxt_p_num)
-      end_num = page_ja.index('<', start_num)
-      @rankTxt_p = page_ja.slice(start_num+1..end_num-1)
-
-    elsif rank2_num > color_num
-      @rankTxt = "1位"
-
-      mTxt_num = page_ja.index('mTxt', month_num)
-      mTxt_p_num = page_ja.index('p', mTxt_num)
-      start_num = page_ja.index('>', mTxt_p_num)
-      end_num = page_ja.index('<', start_num)
-      @rankTxt_p = page_ja.slice(start_num+1..end_num-1)
-
+    if month_row1_rank_index_end
+      @rank  = page_ja.slice(month_row1_rank_index_start + 1 .. month_row1_rank_index_end - 1).to_i
+    elsif tyosukkirisu_index < month_index
+      @rank = 1
     else
-      @rankTxt = "12位"
-
-      mTxt_num = page_ja.index('mTxt', month_num)
-      mTxt_p_num = page_ja.index('p', mTxt_num)
-      start_num = page_ja.index('>', mTxt_p_num)
-      end_num = page_ja.index('<', start_num)
-      @rankTxt_p = page_ja.slice(start_num+1..end_num-1)
+      @rank = 12
     end
-    Sukkirisu.create(month: month, rank: @rankTxt, text: @rankTxt_p, lucky_color: @color)
+    @text  = page_ja.slice(month_row2_p_index_start + 1 .. month_row2_p_index_end - 1)
+    @color = page_ja.slice(month_row2_div_index_start + 1 .. month_row2_div_index_end - 1)
+
+    Sukkirisu.create(month: month, rank: @rank, text: @text, lucky_color: @color)
   end
 end
