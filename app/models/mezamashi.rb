e1bc467e5eb0daa3e49315411a3d1f3681061c0b
seiya endo <seiya.endo0830@gmail.com>
2020-08-31 09:23:46 +0900

    <br>を削除

--- app/models/mezamashi.rb
+++ app/models/mezamashi.rb
@@ -7,7 +7,7 @@ class Mezamashi < ApplicationRecord
     uranaiJson['ranking'].each do |ranking|
       @sign = ranking['name']
       @rank = ranking['rank']
-      @text1 = ranking['text']
+      @text1 = ranking['text'].delete("<br>")
       @lucky_point = ranking['point']
       @advice = ranking['advice']
       Mezamashi.create(sign: @sign, rank: @rank, text1: @text1, lucky_point: @lucky_point, advice: @advice)
